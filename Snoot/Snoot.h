56bd542370ac767c8aeb328cfde822cdefb65fce
Sourav Ray <me@raysourav.com>
2016-08-13 08:10:19 +0530

    Initial Commit

---
+++ Snoot/Snoot.h
@@ -0,0 +1,13 @@
+//
+//  Snoot.h
+//  Snoot
+//
+//  Created by Sourav Ray on 8/13/16.
+//  Copyright © 2016 Sourav Ray. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Snoot : NSObject
+
+@end
---
+++ Snoot/Snoot.m
@@ -0,0 +1,13 @@
+//
+//  Snoot.m
+//  Snoot
+//
+//  Created by Sourav Ray on 8/13/16.
+//  Copyright © 2016 Sourav Ray. All rights reserved.
+//
+
+#import "Snoot.h"
+
+@implementation Snoot
+
+@end
